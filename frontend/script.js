aa1334c9cbde27c28721650281ba65bceb08345a
Jiahao Xu <caprilance.xu@foxmail.com>
2023-04-22 15:54:07 -0400

    Fix: COUNT function in characterize distribution

--- frontend/script.js
+++ frontend/script.js
@@ -27,7 +27,7 @@ var hypoGrammar = {
 
 const CDgrm = {
     "hypo": "expr '=' 'number' ",
-    "expr": " 'DIVIDE' '(' 'COUNT' '('  ')' '[' pred ']' ',' 'COUNT' '('  ')' '[' pred ']' ')' | var '[' pred ']'",
+    "expr": " 'DIVIDE' '(' 'COUNT' '('  ')' '[' subpred ']' ',' 'COUNT' '('  ')' '[' pred ']' ')' | var '[' pred ']'",
     "op":"'=' | '<' | '>'",
     "var": "attr | const ",
     "attr": "'Model' | 'MPG' | 'Cylinders' | 'Displacement' | 'Horsepower' | 'Weight' | 'Acceleration' | 'Year' | 'Origin'",
